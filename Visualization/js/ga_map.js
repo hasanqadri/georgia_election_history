16389de6aa374288def253fa7d3063cf2b12311a
user <user@lawn-128-61-21-94.lawn.gatech.edu>
2017-04-10 21:38:24 -0400

    added vote number summary on hover

--- Visualization/js/ga_map.js
+++ Visualization/js/ga_map.js
@@ -28,7 +28,7 @@ d3.json('/Data/Geo/ga.json', function(error, data) {
 	console.log(data);
 	chartData.georgia = topojson.feature(data, data.objects.states);
 	//console.log(chartData)
-    chartData.counties = topojson.feature(data, data.objects.counties);
+  chartData.counties = topojson.feature(data, data.objects.counties);
 
 	svg.append('path')
 	   .datum(chartData.georgia)
@@ -43,6 +43,7 @@ d3.json('/Data/Geo/ga.json', function(error, data) {
 		.attr('d', path)
 		.on('mouseover', function(d){
 			var countyName = d.properties.NAME_2;
+			displayStatistics(countyName);
 			return document.getElementById('name').innerHTML=countyName;
 		});
 
@@ -50,6 +51,51 @@ d3.json('/Data/Geo/ga.json', function(error, data) {
    //    .datum(topojson.mesh(data, data.objects.counties, function(a, b) { return a.properties.NAME_2 !== b.properties.NAME_2;}))
    //    .attr('d', path)
    //    .attr('class', 'county-boundary');
-    
+
 
 });
+
+function displayStatistics(countyName) {
+	// County names that have spaces in them have an underscore instead in the
+	// CSV title. So, we get rid of spaces and replace with underscore.
+	countyName = countyName.replace(' ', '_');
+
+	var csvName = '/Data/2016/20161108__ga__general__' + countyName +
+	 							'__precinct.csv';
+	d3.csv(csvName, function(error, data) {
+		//console.log("opened data csv file!");
+		console.log(data[1]);
+		var groupByOffice = d3.nest()
+													.key(function(d) {return d.office})
+													.entries(data);
+		presCandidateVotes = getVotesByOffice(groupByOffice, 'President of the United States');
+		console.log(presCandidateVotes);
+		var voteSummaryString = ''
+		for (var i = 0; i < presCandidateVotes.length; i++) {
+			candidate = presCandidateVotes[i].candidate;
+			candidateVotes = presCandidateVotes[i].votes;
+			console.log(candidateVotes);
+			voteSummaryString += candidate + ': ' + candidateVotes + '<br>';
+			console.log(voteSummaryString);
+
+		}
+		document.getElementById('voteInfoName').innerHTML = voteSummaryString;
+	});
+
+	// This function takes an array that has all the votes by office of a
+	// certain county.
+	function getVotesByOffice(groupedVotes, office) {
+		for (var i = 0; i < groupedVotes.length; i++) {
+			console.log(groupedVotes[i].key== office);
+			if (groupedVotes[i].key == office) {
+				// The csv files are structured such that rows that have no
+				// 'precinct' values are aggregate rows.
+				return groupedVotes[i].values.filter(function (d) {
+					return d.precinct == ''
+				});
+			}
+		}
+
+	}
+
+}
